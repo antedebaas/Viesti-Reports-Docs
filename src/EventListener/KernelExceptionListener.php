e2f92c7a3f41a3d5b57ff87d93402384b9bb82d2
Ante de Baas <ante@debaas.net>
2024-11-14 01:31:18 +0100

    update licence, fix minor bugs

---
+++ src/EventListener/KernelExceptionListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class KernelExceptionListener
+{
+    private $templating;
+
+    public function __construct(Environment $templating)
+    {
+        $this->templating = $templating;
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+        switch($exception) {
+            case $exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException:
+                $response = new Response(
+                    $this->templating->render('base/error.html.twig', ['page' => array('title'=> 'Not found'), 'message' => "The object you tried to access could not be found."]),
+                    Response::HTTP_NOT_FOUND
+                );
+                break;
+            case $exception instanceof \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException:
+                $response = new Response(
+                    $this->templating->render('base/error.html.twig',['page' => array('title'=> 'Unauthorized'), 'message' => "Nope! Access denied!"]),
+                    Response::HTTP_UNAUTHORIZED
+                );
+                break;
+            default:
+                $response = new Response(
+                    $this->templating->render('base/error.html.twig',['page' => array('title'=> 'Error'), 'message' => $exception->getMessage()]),
+                    Response::HTTP_INTERNAL_SERVER_ERROR
+                );
+                break;
+        }
+        $event->setResponse($response);
+        $event->stopPropagation();
+    }
+}
--- update.sh
+++ update.sh
@@ -13,18 +13,6 @@ export COMPOSER_ALLOW_SUPERUSER=1; composer install
 echo "clear cache"
 php bin/console cache:clear
 
-if [ -f .env.local ]; then
-    echo "run migrations"
-    php bin/console doctrine:migrations:migrate --no-interaction --query-time --all-or-nothing
-else
-    echo "skipping migrations as there is no .env.local file yet."
-    echo "Create one this by running setup"
-    echo ""
-fi
-
-echo "run upgrade script"
-bash upgradefiles/upgrade.sh
-
 echo "install assets"
 php bin/console assets:install public
 
